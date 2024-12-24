a6eadd603db9658b3bae59c10956d6175ba63792
uday-bhatiya <udaybhatiya31@gmail.com>
2024-12-24 21:58:52 +0530

    add update profile and check route for user authentication

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import UserModel from "../models/user.model.js";
 import { generateToken } from '../lib/utils.js';
 import bcrypt from 'bcryptjs';
+import cloudinary from '../lib/cloudinary.js';
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -114,7 +115,48 @@ export const logout = (req, res) => {
             message: "Logged out successfully"
         });
     } catch (error) {
-        console.log("Error in logout controller", error.message);
+        console.error("Error in logout controller", error.message);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+};
+
+export const updateProfile = async (req, res) => {
+    try {
+        const { profilePic } = req.body;
+        const userId = req.user._id;
+
+        if (!profilePic) {
+            return res.status(400).json({
+                success: false,
+                message: "Profile pic is required"
+            });
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            userId,
+            { profilePic: uploadResponse.secure_url },
+            { new: true }
+        );
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.error("error in update profile:", error);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        });
+    }
+};
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.error("Error in checkAuth controller", error);
         res.status(500).json({
             success: false,
             message: "Internal Server Error"
