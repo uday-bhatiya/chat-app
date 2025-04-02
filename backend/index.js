35a45d328a83df10f41fd2ae0a4b32828ed3a8ca
uday-bhatiya <udaybhatiya31@gmail.com>
2025-04-02 12:44:30 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -9,19 +9,24 @@ import { connectDB } from './src/lib/mongoose.js';
 import authRoutes from './src/routes/auth.routes.js';
 
 import messageRoutes from './src/routes/message.routes.js';
+import path from 'path';
+// import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
 
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
 app.use('/api/v1/auth', authRoutes);
 
 app.use('/api/v1/messages', messageRoutes);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+  });
 
 server.listen(process.env.PORT || 8000, () => {
     console.log(`Server is running on port : ${process.env.PORT}`);
